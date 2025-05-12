cb12c813b74dbc50e75aca1a4a4f294da9a1c962
jdhagood <145863716+jdhagood@users.noreply.github.com>
2025-05-12 02:20:57 -0400

    build test 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/wire_wiz/',  // <-- THIS MUST MATCH YOUR REPO NAME
+  plugins: [react()]
 })
